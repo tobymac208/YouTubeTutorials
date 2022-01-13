3e0d7a8ea1e1edbd8be42fb7b6939497cb7af98f
Nik Fernandez <niklasfernandez97@gmail.com>
2022-01-13 15:03:11 -0600

    First project.

---
+++ javascript_credit_card_validation/index.js
@@ -0,0 +1,31 @@
+const luhnAlgorithm = (ccNumber) => {
+    const length = ccNumber.length;
+    let count = 0;
+
+    /** Traverse the whole credit card number. If index + 1 is even, double the value. 
+     If above 9 then adjust value.*/
+    for(let i = 0; i < length; i++) {
+        let currentDigit = parseInt(ccNumber[i]);
+
+        if ((i+2) % 2 == 0)
+            if((currentDigit *= 2) > 9)
+                currentDigit -= 9;
+        
+        count += currentDigit;
+    }
+
+    return (count % 10) === 0;
+}
+
+const checkCC = () => {
+    const elCCNumber = document.getElementById('ccNumber');
+    const elCCValidation = document.getElementById('ccValidator');
+    let message = "";
+
+    if( luhnAlgorithm(elCCNumber.value) )
+        message = "Well, it looks like that worked! Your CC is valid!";
+    else
+        message = "CC verification failed :(";
+    
+    elCCValidation.textContent = message;
+};
