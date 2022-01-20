3779fabfcdc3b0e98a7292935d1f29740e84821f
Niklas Fernandez <niklas_fernandez@comcast.net>
2022-01-20 12:29:36 -0600

    Adds arrays tutorial.

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -2,17 +2,21 @@
 #include <string>
 using namespace std;
 
-int main(int argc, char** argv) {
-	const int SIZE = 10;
-	int* arr = new int[SIZE];
-	
-	for(int i = 0; i < SIZE; i++){
-		arr[i] = 42;
-		cout << arr[i] << " is number " << (i+1) << endl;
+int main(int argc, char** argv)
+{
+	const int SIZE = 5;
+	int* arrayOfInts = (int *)malloc(sizeof(int) * SIZE);
+
+	for(int i = 0; i < SIZE; i++)
+	{
+		arrayOfInts[i] = 42;
+		cout << arrayOfInts[i] << " is the value of number " << (i+1) << endl;
+		cout << &(arrayOfInts[i]) - &(arrayOfInts[0]) << endl;
 
 		cin.get();
 	}
 
-	delete arr;
+	delete arrayOfInts;
+
 	return 0;
 }
---
+++ arrays/arrays.py
@@ -0,0 +1,9 @@
+SIZE = 10
+arrayOfInts = []
+
+for i in range(SIZE):
+    arrayOfInts.append(42)
+    print(f'{arrayOfInts[i]} is the value for number {i+1}')
+
+    input('')
+
