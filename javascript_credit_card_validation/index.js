59c5dcd69c6b500391df37092d8577fbb7ea089e
tobymac208 <niklas_fernandez@comcast.net>
2022-10-17 01:01:10 -0500

    Adds checking for if the length is even or odd. Accidentally wrote a large bug into the code last time.

--- javascript_credit_card_validation/index.js
+++ javascript_credit_card_validation/index.js
@@ -4,26 +4,52 @@ const luhnAlgorithm = (ccNumber) => {
     const length = ccNumber.length;
     let count = 0;
 
-    /** Traverse the whole credit card number.
-     *  Starts at the end of the number and begins doubling from the second-to-last number. This fixes the case for odd-numbered length credit card numbers, like AMEX cards.
-    */
-    for(let i = length - 1 ; i >= 0; i--)
+    if(length % 2 == 0)
     {
-        let currentDigit = parseInt(ccNumber[i]);
-        if ((i - 1) % 2 == 0) // I only want to double every other number, starts doubling with the second-to-last number. I don't want to double the last number.
+        /** Traverse the whole credit card number.
+         *  Starts at the end of the number and begins doubling from the second-to-last number. This fixes the case for odd-numbered length credit card numbers, like AMEX cards.
+        */
+        for(let i = 0; i < length; i++)
         {
-            if ((currentDigit *= 2) > 9)
+            let currentDigit = parseInt(ccNumber[i]);
+            if (i % 2 == 0) // I only want to double every other number, starts doubling with the second-to-last number. I don't want to double the last number.
             {
-                // Separate the number into component parts and then add them together.
-                let trailingNumber = currentDigit % 10;
-                let firstNumber = parseInt(currentDigit / 10);
+                if ((currentDigit *= 2) > 9)
+                {
+                    // Separate the number into component parts and then add them together.
+                    let trailingNumber = currentDigit % 10;
+                    let firstNumber = parseInt(currentDigit / 10);
 
-                // If currentDigit was 18 then currentDigit is now 9.
-                currentDigit = firstNumber + trailingNumber;
+                    // If currentDigit was 18 then currentDigit is now 9.
+                    currentDigit = firstNumber + trailingNumber;
+                }
             }
+            
+            count += currentDigit;
         }
-        
-        count += currentDigit;
+    }
+    else {
+        /** Traverse the whole credit card number.
+         *  Starts at the end of the number and begins doubling from the second-to-last number. This fixes the case for odd-numbered length credit card numbers, like AMEX cards.
+        */
+        for(let i = length - 1 ; i >= 0; i--)
+        {
+            let currentDigit = parseInt(ccNumber[i]);
+            if ((i - 1) % 2 == 0) // I only want to double every other number, starts doubling with the second-to-last number. I don't want to double the last number.
+            {
+                if ((currentDigit *= 2) > 9)
+                {
+                    // Separate the number into component parts and then add them together.
+                    let trailingNumber = currentDigit % 10;
+                    let firstNumber = parseInt(currentDigit / 10);
+
+                    // If currentDigit was 18 then currentDigit is now 9.
+                    currentDigit = firstNumber + trailingNumber;
+                }
+            }
+            
+            count += currentDigit;
+        }
     }
 
     return (count % 10) === 0;
